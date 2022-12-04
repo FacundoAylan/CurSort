b854e80a8ce3d47d0dac17d6a99b4c1c74568744
Mailen Rocio Benitez <mailenbenitez93@gmail.com>
2022-12-04 17:21:13 -0300

    Try fix Routes/index

--- API/src/Routes/index.js
+++ API/src/Routes/index.js
@@ -1,16 +1,33 @@
 const { Router } = require('express');
 const axios = require('axios');
-const courseRoutes = require('./courses');
-const userRoutes = require('./users');
 
 const router = Router();
 
+
+const courseRoutes = require('./courses');
+const userRoutes = require('./users');
 
 router.use('/course', courseRoutes);
 router.use('/users', userRoutes);
 
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
