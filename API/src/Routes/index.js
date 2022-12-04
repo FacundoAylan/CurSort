d13083fcde4f64515a7370797a584c4e8b4b0814
Mailen Rocio Benitez <mailenbenitez93@gmail.com>
2022-12-04 12:45:12 -0300

    feat/Routes/index: Add route /users

--- API/src/Routes/index.js
+++ API/src/Routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const courseRoutes = require('./courses');
+const userRoutes = require('./users');
 
 
 router.use('/course', courseRoutes);
+router.use('/users', userRoutes);
 
 
 module.exports = router;
