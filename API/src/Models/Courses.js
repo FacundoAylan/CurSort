d48be3dc4e77c7f4e7bdd4232a5856827ac2114a
MLSalerno <mlslssl1991@gmail.com>
2022-12-05 15:56:37 -0300

    post de reseñas, ruta para traer todos los cursos o busqueda por query, ruta provisoria de carga cursos JSON a DB

--- API/src/Models/Courses.js
+++ API/src/Models/Courses.js
@@ -17,12 +17,20 @@ module.exports = (sequelize) => {
     duration: {
       type: DataTypes.INTEGER,
     },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
     rating: {
       type: DataTypes.FLOAT,
       default: 0,
     },
     image: {
       type: DataTypes.STRING
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
     },
     difficulty: {
       type: DataTypes.STRING,
