697fd3764c0ec385c2abe7c13085a7368288d028
jona <jojhan91@gmail.com>
2022-12-05 19:01:31 -0500

    ruta categorias

--- API/src/Routes/index.js
+++ API/src/Routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const routeCourses = require('./courses.js')
 const userRoutes = require('./users');
+const categorias = require('./categorias');
 const router = Router();
 
 
@@ -8,6 +9,7 @@ const router = Router();
 // router.use(express.json())
 router.use('/courses', routeCourses)
 router.use('/users', userRoutes);
+router.use('/categories', categorias);
 
 
 module.exports = router;
