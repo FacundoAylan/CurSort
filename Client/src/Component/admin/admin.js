12aa8349a1053c06b9daab1e2680aa7e7b9dc146
FacundoAylan <facundogsb@gmail.com>
2022-12-31 15:16:24 -0300

    admin FA

--- Client/src/Component/admin/admin.js
+++ Client/src/Component/admin/admin.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Flex, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+import { Box, Flex, IconButton, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
 import Usuarios from './usuarios/usuarios';
 import SearchAdmin from './searchAdmin/searchAdmin';
 import CoursesAdmin from './coursesAdmin/coursesAdmin'
 import ListCourses from './listUsers/listusers'
+import { Link } from 'react-router-dom';
+import { ArrowLeftIcon } from '@chakra-ui/icons';
 
 
 function Admin () {
@@ -11,42 +13,53 @@ function Admin () {
 
   return (
     <>
-    <Tabs>
-      <TabList color='white'>
-        <Tab>Users</Tab>
-        <Tab>ListUsers</Tab>
-        <Tab>Courses</Tab>
-        <Tab>statistics</Tab>
-      </TabList>
+      <Box pt="10px">
+        <Link to="/">
+          <IconButton
+            colorScheme="blue"
+            aria-label="Search database"
+            icon={<ArrowLeftIcon />}
+          />
+        </Link>
+      </Box>
+      <Tabs>
+        <TabList color="white">
+          <Tab>Users</Tab>
+          <Tab>ListUsers</Tab>
+          <Tab>Courses</Tab>
+          <Tab>statistics</Tab>
+        </TabList>
 
-      <TabPanels>
-        <TabPanel>
-          <SearchAdmin/>  
-          <Flex flexDirection='row' flexWrap='wrap' justifyContent='space-around' maxW='90%' pl='120px'>
-            {
-            usuario.map(value =>{
-              return (
-                <Usuarios/>
-              )
-            })
-            }
-          </Flex>
-        </TabPanel>
-        <TabPanel>
-          <ListCourses/>
-        </TabPanel>
+        <TabPanels>
+          <TabPanel>
+            <SearchAdmin />
+            <Flex
+              flexDirection="row"
+              flexWrap="wrap"
+              justifyContent="space-around"
+              maxW="90%"
+              pl="120px"
+            >
+              {usuario.map((value) => {
+                return <Usuarios />;
+              })}
+            </Flex>
+          </TabPanel>
+          <TabPanel>
+            <ListCourses />
+          </TabPanel>
 
-        <TabPanel>
-          <CoursesAdmin/>
-        </TabPanel>
+          <TabPanel>
+            <CoursesAdmin />
+          </TabPanel>
 
-        <TabPanel>
-          <p>three!</p>
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </>
-  )
+  );
 }
 
 export default Admin;
