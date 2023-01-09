b5b0d5883064a3060c77805b2dcac3d2c521ca0a
Facundo Aylan <87158215+FacundoAylan@users.noreply.github.com>
2023-01-08 23:50:05 -0300

    Update Information.js

--- Client/src/Component/ShoppingCart/Information.js
+++ Client/src/Component/ShoppingCart/Information.js
@@ -39,7 +39,7 @@ function Information() {
               country: formData.get('country'),
               postalCode: formData.get('cp'),
           };
-          const { data } = await axios.post('http://localhost:3001/checkout/information', buyer);
+          const { data } = await axios.post('https://cursort.onrender.com//checkout/information', buyer);
           //console.log(data);
           if(data.message === 'success'){
             //console.log(data)
