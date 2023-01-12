db01753d24b4611cdf7865a0e6e3c1672939d9d0
Mailen Rocio Benitez <mailenbenitez93@gmail.com>
2023-01-12 10:57:38 -0300

    cambios pull

--- API/src/app.js
+++ API/src/app.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./Routes/index.js');
 
-// require('./db.js');
 
 const server = express();
 
@@ -15,6 +14,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
+
 res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
 res.header('Access-Control-Allow-Credentials', 'true');
 res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
