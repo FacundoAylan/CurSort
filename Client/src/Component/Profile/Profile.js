4e83c862991c0c86bb4740f2169099dfc5926e3e
Facundo Aylan <87158215+FacundoAylan@users.noreply.github.com>
2023-01-13 02:54:02 -0300

    Update Profile.js

--- Client/src/Component/Profile/Profile.js
+++ Client/src/Component/Profile/Profile.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import { Button, Center, Grid, GridItem } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import CreateCategory from '../admin/CreateCategory/CreateCategory';
+import { Link , useHistory} from 'react-router-dom';
+import {useDispatch} from 'react-redux'
+import { getUserEmail } from '../../Redux/actions';
+
 
 const Profile = () => {
+	// const { user, isAuthenticated } = useAuth0();
+	//console.log('isAuthenticated', isAuthenticated)
 	//LocalStore
 	  const user = JSON.parse(window.localStorage.getItem("user"));
 	  const loguin = JSON.parse(window.localStorage.getItem("loguin"));
+  const dispatch = useDispatch()
+  const history = useHistory()
+  const handleOnClick = (e)=>{
+    e.preventDefault()
+    dispatch(getUserEmail(user.email)) 
+    history.push('/editprofile')
+  }
 
 	return (
 		<Grid templateRows='25px repeat(2,45px)'>
@@ -13,19 +27,23 @@ const Profile = () => {
 				<GridItem>{loguin && <h2>{user.name}</h2>}</GridItem>
 			</Center>
 
-			<Button
-				background='black'
-				color='white'
-				border='2px'
-				borderColor='white'
-				borderRadius='12px'
-				ml='10px'
-				width='90%'
-			>
-				Settings
-			</Button>
+			
+      <Link to='/editprofile'>
+        <Button
+          onClick={handleOnClick}
+          background='black'
+          color='white'
+          border='2px'
+          borderColor='white'
+          borderRadius='12px'
+          ml='10px'
+          width='90%'
+        >
+          Settings
+        </Button>
+      </Link>
 
-			<Link to='/cursos'>
+			<Link to='/mycourses'>
 				<Button
 					background='black'
 					color='white'
@@ -38,6 +56,8 @@ const Profile = () => {
 					My courses
 				</Button>
 			</Link>
+
+		
 		</Grid>
 	);
 };
